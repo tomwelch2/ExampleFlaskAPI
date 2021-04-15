bb6b666b68e3a354dc4fead485bd0462dc6b51d3
tomwelch2 <tomwelch2002@gmail.com>
2021-04-15 14:28:10 +0100

    Updated env.groovy

--- env.groovy
+++ env.groovy
@@ -1,3 +1,3 @@
-env.EC2_PUBLIC_IP="ENTER PUBLIC IP HERE"
-env.pkey="Enter the key file for EC2 instance HERE"
-env.sshcredentials="ENTER SSH CREDENTIALS FROM JENKINS HERE"
+env.EC2_PUBLIC_IP="52.56.80.209"
+env.pkey="airflowkey.pem"
+env.sshcredentials="newserver"
